52488fbbb315b142c4d0335532bedd86541c46f2
Ra1nningDay <123659453+Ra1nningDay@users.noreply.github.com>
2025-03-08 20:59:19 +0700

    fixed: type in interface and set map to use Type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
 import axios from "axios";
-
+import { Task } from "../types/task";
+    
 export default async function Home() {
-    const res = await axios("http://localhost:3000/api/tasks");
+    const res = await axios.get("http://localhost:3000/api/tasks", {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
 
-    return <h1>{text}</h1>;
+    const tasks = res.data;
+
+    return (
+        <div>
+            <h1>Tasks</h1>
+            <ul>
+                {tasks.map((task: Task) => (
+                    <li key={task.id}>
+                        {task.name} - {task.completed ? "Done" : "Pending"}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
