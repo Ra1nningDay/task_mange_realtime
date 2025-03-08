cbf72577f8855b12956e9b1c7b3dca1fbbbabcd5
Ra1nningDay <123659453+Ra1nningDay@users.noreply.github.com>
2025-03-09 01:06:17 +0700

    built: create req to del a task

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { useState, useEffect } from "react";
 export default function Home() {
     const [tasks, setTasks] = useState<Task[]>([]);
     const [newTask, setNewTask] = useState("");
+    const [isEdit, setEdit] = useState<boolean>(false);
 
     useEffect(() => {
         const fetchTask = async () => {
@@ -26,29 +27,63 @@ export default function Home() {
                 name: newTask,
                 completed: false,
             });
-            setTasks([...tasks, res.data]);
+            setTasks((prevTasks) => [...prevTasks, res.data]);
             setNewTask("");
         } catch (err) {
             console.error("Error Adding Task", err);
         }
     }
 
+    async function handleDel(id) {
+        try {
+            const res = await axios.delete(
+                `http://localhost:3000/api/tasks/${id}`
+            );
+            setTasks((prevTasks) => [...prevTasks, res.data]);
+        } catch (err) {
+            console.error("Error to delete task", err);
+        }
+    }
+
     return (
-        <div>
-            <h1>Tasks</h1>
-            <input
-                type="text"
-                onChange={(e) => setNewTask(e.target.value)}
-                value={newTask}
-            />
-            <button onClick={handleAdd}>Add Task</button>
-            <ul>
-                {tasks.map((task: Task) => (
-                    <li key={task.id}>
-                        {task.name} - {task.completed ? "Done" : "Pending"}
-                    </li>
-                ))}
-            </ul>
+        <div className="max-w-2xl mx-auto border-2 p-4 my-8">
+            <h1 className="text-center text-4xl font-bold mb-6">
+                Task Managaement
+            </h1>
+            <div className="flex">
+                <input
+                    type="text"
+                    className="bg-white text-black text-2xl me-2 w-full"
+                    onChange={(e) => setNewTask(e.target.value)}
+                    value={newTask}
+                />
+                <button
+                    type="button"
+                    className="dark:bg-white cursor-pointer dark:hover:bg-black dark:hover:text-white border-2 dark:text-black"
+                    onClick={handleAdd}
+                >
+                    Add Task
+                </button>
+            </div>
+            {tasks.map((task: Task) => (
+                <div
+                    key={task.id}
+                    className="flex flex-column justify-between p-4 border-2 mt-3"
+                >
+                    <p>{task.name}</p>
+                    <div className="flex">
+                        <button className="me-2 cursor-pointer">
+                            Complete
+                        </button>
+                        <button
+                            className="cursor-pointer"
+                            onClick={() => handleDel(task.id)}
+                        >
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 }
